f5f05c3a3c56bee2d87c698226cb6e290322593e
Valdemir Júnior <134114016+Valdemir-DSW@users.noreply.github.com>
2025-11-22 14:14:38 -0300

    Update ExternalADC.ino

--- examples/ExternalADC/ExternalADC.ino
+++ examples/ExternalADC/ExternalADC.ino
@@ -3,6 +3,8 @@
 VALMUX16 mux(2,3,4,5);
 
 int fakeADC() {
+    //Você pode utilizar, para utilizar uma fonte de ADC externa, por exemplo, o módulo ADS1115. Com essa função será totalmente possível utilizar. 
+    //Neste caso, a porta analógica comum dos multiplexadores será ligada em apenas uma porta, no módulo ADS, por exemplo. Daí aqui, nesta função, você faria a leitura desta porta em específico.
     return 1234;
 }
 
